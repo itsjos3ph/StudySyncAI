ecd804efe254b5e7aa1c7fe70e606056e130a911
Joseph Sabo <jsephsabo@gmail.com>
2025-02-24 22:31:18 -0700

    done

--- app.py
+++ app.py
@@ -215,5 +215,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
